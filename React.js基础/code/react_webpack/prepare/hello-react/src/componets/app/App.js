9cef55fcb9a0a27ff140ba6e95590462f3e5a925
qilei <qiilee@126.com>
2018-05-22 00:26:52 +0800

    Fix: Add my-add

--- React.js基础/code/react_webpack/prepare/hello-react/src/componets/app/App.js
+++ React.js基础/code/react_webpack/prepare/hello-react/src/componets/app/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Hello React333!</h2>
+          <h2>Hello React!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ React.js基础/code/react_webpack/prepare/my-app/readme.txt
@@ -0,0 +1,3 @@
+created by qilei
+
+使用create-react-app脚手架进行构建
