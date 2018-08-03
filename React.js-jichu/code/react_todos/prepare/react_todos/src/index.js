d8aa13fec3b80b1d9f6f8de7d56ceea548d0ec3c
齐磊 <qiilee@126.com>
2018-08-03 09:04:24 +0800

    New：note

--- React.js-jichu/code/react_todos/prepare/react_todos/src/index.js
+++ React.js-jichu/code/react_todos/prepare/react_todos/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-
+// <App /> 有空格
 ReactDOM.render(
   <App />,
   document.getElementById('root')
