40e43ce1761f4871fb6e41ed4497bc6a031bda28
齐磊 <qiilee@126.com>
2018-08-27 15:23:46 +0800

    New：运行成功

---
+++ qilei/01/src/components/ToDoApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ToDoApp extends React.Component {
+  render() {
+    return (
+      <div>To Do App</div>
+    );
+  }
+}
+
+export default ToDoApp;
--- qilei/01/webpack.config.js
+++ qilei/01/webpack.config.js
@@ -1,27 +1,26 @@
 var webpack = require('webpack');
 module.exports = {
-  entry: './src/app.js',
-  output: {
-      path: __dirname + '/build',
-      filename: "bundle.js"
-  },
-  module: {
-      rules: [{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-              plugins: ['transform-runtime'],
-              presets: ['es2015', 'react', 'stage-2']
-          }
-      }, {
-          test: /\.css$/,
-          loader: "style-loader!css-loader"
-      }]
-  }
+    entry: [
+        // 'babel-polyfill',
+        // 'webpack/hot/only-dev-server',
+        './src/app.js'
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                plugins: ['transform-runtime'],
+                presets: ['es2015', 'react', 'stage-2']
+            }
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    }
 };
-
-作者：瘦人假噜噜
-链接：https://www.jianshu.com/p/324fd1c124ad
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
