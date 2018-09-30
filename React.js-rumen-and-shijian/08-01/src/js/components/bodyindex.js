324a27cbcf7020328d27933f19d205619936c0f8
qilei <qiilee@126.com>
2018-09-30 08:23:05 +0800

    Update: learning-react

--- React.js-rumen-and-shijian/08-01/src/js/components/bodyindex.js
+++ React.js-rumen-and-shijian/08-01/src/js/components/bodyindex.js
@@ -1,21 +1,43 @@
+// import React from 'react';
+// export default class BodyIndex extends React.Component {
+// 	constructor() {
+// 		super(); //调用基类的所有的初始化方法
+// 		this.state = {
+// 			username: "Parry",
+// 			age: 20
+// 		}; //初始化赋值
+// 	}
+// 	render() {
+// 		setTimeout(() => {
+// 			//更改 state 的时候
+// 			this.setState({username: "IMOOC", age: 30});
+// 		}, 4000);
+// 		return (
+// 			<div>
+// 				<h2>页面的主体内容</h2>
+// 				<p>{this.state.username} {this.state.age}</p>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// 1
 import React from 'react';
-export default class BodyIndex extends React.Component {
+export default class BodyIndex extends React.Component{
 	constructor() {
-		super(); //调用基类的所有的初始化方法
+		super();
 		this.state = {
-			username: "Parry",
-			age: 20
-		}; //初始化赋值
+			username:'qilei 四秒变 sainan'
+		}
 	}
-	render() {
+	render () {
 		setTimeout(() => {
 			//更改 state 的时候
-			this.setState({username: "IMOOC", age: 30});
+			this.setState({username: "sainan"});
 		}, 4000);
 		return (
 			<div>
-				<h2>页面的主体内容</h2>
-				<p>{this.state.username} {this.state.age}</p>
+				<span>主体 name:</span><span>{this.state.username}</span>
 			</div>
 		)
 	}
--- React.js-rumen-and-shijian/08-01/src/js/components/header.js
+++ React.js-rumen-and-shijian/08-01/src/js/components/header.js
@@ -1,10 +1,23 @@
+// import React from 'react';
+// export default class ComponentHeader extends React.Component{
+//   render(){
+//     return (
+//       <header>
+//         <h1>这里是头部</h1>
+//       </header>
+//     )
+//   }
+// }
+
+
+// 1
 import React from 'react';
-export default class ComponentHeader extends React.Component{
+export default class ComponentHeader extends React.Component {
   render(){
-    return (
+    return(
       <header>
-        <h1>这里是头部</h1>
-      </header>
+        <h1>qilei de tou</h1>
+      </header>  
     )
   }
 }
