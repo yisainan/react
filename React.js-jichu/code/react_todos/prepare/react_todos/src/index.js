41b666cb835be1a6916afe14ee275e7fb51455d9
qilei <qiilee@126.com>
2019-02-14 13:39:55 +0800

    fix: 整理React.js-jichu目录

--- React.js-jichu/code/react_todos/prepare/react_todos/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-// <App /> 有空格
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
